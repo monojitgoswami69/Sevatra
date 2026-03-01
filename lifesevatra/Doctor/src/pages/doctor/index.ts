d52fba2d496257edd87fb75c7447cc2c4bec3b17
monojitgoswami69 <monojit.goswami.0905@gmail.com>
2026-03-01 06:32:36 +0530

    dir upd

--- lifesevatra/Doctor/src/pages/doctor/index.ts
+++
@@ -1,6 +0,0 @@
-export { default as DoctorDashboard } from './DoctorDashboard';
-export { default as MyPatients } from './MyPatients';
-export { default as PatientUpdate } from './PatientUpdate';
-export { default as DoctorProfile } from './DoctorProfile';
-export { default as Schedule } from './Schedule';
-export { default as ClinicalNotes } from './ClinicalNotes';
--- lifesevatra/Frontend/src/pages/dashboard/index.ts
+++
@@ -1,5 +0,0 @@
-export { default as Dashboard } from './Overview';
-export { default as NewAdmission } from './NewAdmission';
-export { default as Staff } from './Staff';
-export { default as NewStaff } from './NewStaff';
-
